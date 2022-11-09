b707295964b047efd65b4247e42b32a0045aa086
Ismael Cobo <midismael@gmail.com>
2022-11-09 14:05:51 +0100

    first steps nodejs ts

---
+++ back/models/server.ts
@@ -0,0 +1,15 @@
+import express, { Application } from 'express'
+
+export class Server {
+  private app: Application
+  private port: string
+
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT || '3000'
+  }
+
+  listen() {
+    this.app.listen(this.port, () => console.log(`Listenig on port ${this.port}`))
+  }
+}
