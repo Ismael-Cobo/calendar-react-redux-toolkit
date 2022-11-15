6375f78b67ee321c6962c5b283db14884ed5c322
Ismael Cobo <midismael@gmail.com>
2022-11-15 12:55:03 +0100

    feat: make interface better with sequelize

--- back/interfaces/User.ts
+++ back/interfaces/User.ts
@@ -1,7 +1,11 @@
-export interface UserInterface {
-  id: string
+import { Optional, Model, InferAttributes, InferCreationAttributes } from 'sequelize'
+
+export interface UserInterface extends Model<InferAttributes<UserInterface>, InferCreationAttributes<UserInterface>> {
+  id?: string
   name: string
   email: string
   password: string
   state: boolean
 }
+
+export type UserCreationAttributes = Optional<UserInterface, 'id'>
