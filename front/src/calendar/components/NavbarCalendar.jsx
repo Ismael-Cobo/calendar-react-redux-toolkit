d4c152a1dcc0e94a848e748f9a2bd1dc5d4abc53
Ismael Cobo <midismael@gmail.com>
2022-11-22 14:04:16 +0100

    feat: name and logout functionality

--- front/src/calendar/components/NavbarCalendar.jsx
+++ front/src/calendar/components/NavbarCalendar.jsx
@@ -1,14 +1,17 @@
 import { Button, Container, Navbar } from 'react-bootstrap'
+import { useAuthStore } from '../../hooks'
 
 export const NavbarCalendar = () => {
+  const { user, startLogOut } = useAuthStore()
+
   return (
     <Navbar className='mb-4 px-4' bg='dark' variant='dark' expand='lg'>
       <Container>
         <Navbar.Brand>
           <i className='fas fa-calendar-alt'></i>
-          &nbsp; Ismael
+          &nbsp; {user.name}
         </Navbar.Brand>
-        <Button variant='outline-danger'>
+        <Button variant='outline-danger' onClick={startLogOut}>
           <i className='fas fa-sign-out-alt'></i>
           Salir
         </Button>
